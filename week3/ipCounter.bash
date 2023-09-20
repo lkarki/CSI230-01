af328ffbae6935ed570f2a9b07054ffea9ff91df
Lucius Karki <lucius.karki@mymail.champlain.edu>
2023-09-20 14:36:55 -0400

    fixed IP Counter assignment

--- week3/ipCounter.bash
+++ week3/ipCounter.bash
@@ -15,8 +15,8 @@ ip3binary=$(echo "obase=2;$myip3" | bc)
 ip4binary=$(echo "obase=2;0" | bc)
 for i in {1..254}
 do
-printf '%08d'$ip1binary $ip2binary $ip3binary $ip4binary
-printf $
+printf '%08d'$ip1binary $ip2binary $ip3binary $ip4binary >> ipCounterOutput.txt
+printf "\n" >> ipCounterOutput.txt
 ip4binary=$(echo "obase=2;$i" | bc)
 done
 fi
