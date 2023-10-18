e5ad15fc5d23ee0236d3ec96faa5e7951e670fb7
lkarki <lucius.karki@mymail.champlain.edu>
2023-10-18 13:25:10 -0400

    finished first powersell script assignment

---
+++ week8/fps.ps1
@@ -0,0 +1,8 @@
+﻿cd C:\Users\champuser\Desktop
+
+clear
+
+Get-Eventlog -list
+
+$readLog = Read-host -Prompt "Please select a log to review"
+Get-EventLog -LogName $readLog -Newest 3 | Export-Csv -NoTypeInformation -Path "rep.csv"
