0256ecf213961e774bdb3c4777010955d94ad208
lkarki <lucius.karki@mymail.champlain.edu>
2023-11-01 14:41:37 -0400

    Finished second assignment

---
+++ week10/Week10Assignment2.ps1
@@ -0,0 +1,5 @@
+﻿Set-Location -Path C:\Users\champuser\CSI230-01
+$Files=Get-ChildItem -Recurse -Filter "*.bash"
+for($i=0; $i -lt $Files.Length;$i++){
+Get-Content $Files[$i].FullName | Measure-Object -Line -Word
+}
